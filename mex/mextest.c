e306f0e76bf66475275743724e22b12f2dc3e1f7
Joar Bagge <joar.bagge@gmail.com>
2020-10-29 15:12:18 +0100

    Changed the test since printing in a parallel section causes newer Matlab versions to crash

--- mex/mextest.c
+++ mex/mextest.c
@@ -3,17 +3,22 @@
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
+    int sum = 0;
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
     {
-        int num = omp_get_thread_num();
+        //int num = omp_get_thread_num();
 #ifdef _OPENMP
 #pragma omp for
 #endif
         for (int i=0; i<16; i++)
         {
-            printf("Thread %d, iter %d\n", num, i);
+#ifdef _OPENMP
+#pragma omp critical
+#endif
+            sum += i;
         }
     }
+    printf("The sum is %d\n", sum);
 }
